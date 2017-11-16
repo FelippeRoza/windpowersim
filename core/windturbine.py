2eae6c184b89167a9176674c28a968c941da95e9
FelippeRoza <felippesroza@gmail.com>
2017-11-16 14:29:18 -0200

    added print_cp method

--- core/windturbine.py
+++ core/windturbine.py
@@ -1,5 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import math
-from math import exp
 
 class turbine(object):
     """docstring for turbine."""
@@ -9,11 +10,13 @@ class turbine(object):
         self.ang_speed_unit = ang_speed_unit
         self.wind_speed_unit = wind_speed_unit
         self.rho = 1.225 # air density
+        self.kmh_to_ms = 0.277778 # convert km/h to m/s
+        self.rpm_to_rads = 0.1047 # convert RPM to rad/s
 
-    def cp(self, alfa, beta):
+    def cp(self, alfa):
+        #turbine power coeficient
         #alfa is the Tip Speed Ratio and beta is the pitch angle
-        alfa_i = (1/(alfa + 0.08*beta)) - 0.035/(beta**3 + 1)
-        print('alfa_i ' + str(alfa_i))
+        alfa_i = (1/(alfa + 0.08*self.beta)) - 0.035/(self.beta**3 + 1)
         c1 = 0.5
         c2 = 116*alfa_i
         c3 = 0.4
@@ -21,16 +24,34 @@ class turbine(object):
         c5 = 5
         c6 = 21*alfa_i
         x = 1.5
-        cp = c1*(c2 - c3*beta - c4*(beta**x) - c5) * exp(-c6)
-        print('cp ' + str(cp))
+        cp = c1*(c2 - c3*self.beta - c4*(self.beta**x) - c5) * np.exp(-c6)
         return cp
 
-    def mech_power(self, wind_speed, angular_speed):
-        wind_speed = wind_speed * 0.277778 # km/h to m/s
-        angular_speed = angular_speed * 0.1047 # RPM to rad/s
-        alfa = ((angular_speed * self.radius) / (wind_speed))
-        print('alfa ' + str(alfa))
-        cp = self.cp(alfa, self.beta)
+    def print_cp(self, wind_speed, angular_speed):
+        #wind_speed and angular_speed are pandas Series
+        alfa = self.alfa(wind_speed, angular_speed)
+        cp = self.cp(alfa)
+        plt.plot(alfa,cp)
+        plt.show()
+
+    def available_power(self, wind_speed):
+        #wind kinetic energy
+        if self.wind_speed_unit == 'km/h':
+            wind_speed = wind_speed*self.kmh_to_ms
         A = math.pi*self.radius**2
-        pm = cp * self.rho * A * (wind_speed**3) / 2
+        pw = self.rho * A * (wind_speed**3) / 2
+        return pw
+
+    def alfa(self, wind_speed, angular_speed):
+        if self.ang_speed_unit == 'RPM':
+            angular_speed = angular_speed*self.rpm_to_rads
+        if self.wind_speed_unit == 'km/h':
+            wind_speed = wind_speed*self.kmh_to_ms
+        return ((angular_speed * self.radius) / (wind_speed))
+
+    def mech_power(self, wind_speed, angular_speed):
+        #turbine power
+        alfa = self.alfa(wind_speed, angular_speed)
+        cp = self.cp(alfa)
+        pm = cp * self.available_power(wind_speed)
         return pm
